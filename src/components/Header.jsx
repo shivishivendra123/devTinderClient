593b437a67cdc5c5f6bcf54eaaef26b0ac1cdd23
Shivendra Gupta <gshivendra234@gmail.com>
2025-02-18 16:17:39 -0500

    Notofications fixed but socket loose still

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,41 @@
 import { useDispatch, useSelector } from "react-redux"
 import { Link, Outlet, useNavigate } from "react-router-dom"
 import { addUser } from "./slices/userSlice"
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 import BASE_URL from '../utils/constant'
 import creatSocketConnection from "../utils/socketConnect"
 import { setSocket } from "./slices/socketSlice"
+import { addnotification } from "./slices/notificationSlice"
 
 
-const Header = ()=>{
+const Header = () => {
   const dispatch = useDispatch()
-  
+
   const user_crd = useSelector((store) => store.user)
   const navigate = useNavigate()
 
   const [notification, setNotification] = useState(false)
 
-    const [message,setMessage] = useState("")
-    const notify = useSelector((store) => store.notify)
+  const [message, setMessage] = useState("")
+  const notify = useSelector((store) => store.notify)
 
-    useEffect(() => {
-        if (notify.length > 0) {
-            setNotification(true)
-            setMessage(notify[notify.length - 1])
-            
-            setTimeout(() => {
-                setNotification(false)
-            }, [3000])
-        }
+  useEffect(() => {
+    if (notify.length > 0) {
+      setNotification(true)
+      setMessage(notify[notify.length - 1])
 
-    }, [notify])
+      setTimeout(() => {
+        setNotification(false)
+      }, [3000])
+    }
 
-  const handleLogout = async()=>{
-    
-    const logout = await fetch(BASE_URL+'/v1/logout',{
-      method:'POST',
-      credentials:'include',
+  }, [notify])
+
+  const handleLogout = async () => {
+
+    const logout = await fetch(BASE_URL + '/v1/logout', {
+      method: 'POST',
+      credentials: 'include',
       headers: {
         "Content-Type": "application/json",
       },
@@ -44,78 +45,86 @@ const Header = ()=>{
     navigate('/login')
   }
 
-  const check_auth = async()=>{
-    try{
-      let user_data = await fetch(BASE_URL+'/v1/profile/view',{
-        method:'GET',
-        credentials: "include", 
+  const check_auth = async () => {
+    try {
+      let user_data = await fetch(BASE_URL + '/v1/profile/view', {
+        method: 'GET',
+        credentials: "include",
       })
-      if(user_data.status == 401){
+      if (user_data.status == 401) {
         throw new Error("User not logged In")
       }
       user_data = await user_data.json()
       console.log(user_data.user_found._id)
       dispatch(addUser(user_data.user_found))
       
+      let socket = creatSocketConnection()
+      socket.emit('joinNotificationService', { room: user_data.user_found._id })
+
+      socket.on('connection', ({ notification }) => {
+        console.log(notification)
+        dispatch(addnotification(notification))
+        console.log(notification)
+      })
 
     }
-   
-    catch(err){
+
+    catch (err) {
       navigate('/login')
-        console.log(err.message)
+      console.log(err.message)
     }
   }
 
 
 
-  useEffect(()=>{
+  useEffect(() => {
     check_auth()
-  },[])
+  }, [])
 
-  const user = useSelector((store)=>store.user)
-    return (<>
-      {
-                    notification ? (
-                        <div className="toast toast-top toast-start" id="notification">
-                            <div className="alert alert-info">
-                                <span>{message}</span>
-                            </div>
-                        </div>
-                    ) : null
-                }
-    <div>
-        <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">Hii Dev</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      
-      {user_crd?(<>
-        <li><Link to="/feed">Feed</Link></li>
-        <li><Link to="/connections">My Connections</Link></li>
-      <li><Link to="/Requests">Requests</Link></li>
-      <li><Link to="/chats">Chats</Link></li>
-      </>):null}
-      
-      {user_crd?(<li className="mx-6">
-        <details>
-          <summary>{user_crd?user_crd.firstName || "scs":null}</summary>
-          <ul className="bg-base-100 rounded-t-none p-2">
-          <li><Link to="/profile">Profile</Link></li>
-            <li><a onClick={handleLogout}>Logout</a></li>
-          </ul>
-        </details>
-      </li>):null}
-      
-    </ul>
-  </div>
+  const user = useSelector((store) => store.user)
+  return (<>
+    {
+      notification ? (
+        <div className="toast toast-top toast-start" id="notification">
+          <div className="alert alert-info">
+            <span>{message}</span>
+          </div>
         </div>
-        <Outlet/>
-        
+      ) : null
+    }
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Hii Dev</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+
+            {user_crd ? (<>
+              <li><Link to="/feed">Feed</Link></li>
+              <li><Link to="/connections">My Connections</Link></li>
+              <li><Link to="/Requests">Requests</Link></li>
+              <li><Link to="/chats">Chats</Link></li>
+            </>) : null}
+
+            {user_crd ? (<li className="mx-6">
+              <details>
+                <summary>{user_crd ? user_crd.firstName || "scs" : null}</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><Link to="/profile">Profile</Link></li>
+                  <li><a onClick={handleLogout}>Logout</a></li>
+                </ul>
+              </details>
+            </li>) : null}
+
+          </ul>
+        </div>
+      </div>
+      <Outlet />
+
     </div>
-   
-    </>
+
+  </>
   )
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -87,6 +87,7 @@ function Login() {
             console.log(result.user_cred)
             let socket = creatSocketConnection()
             socket.emit('joinNotificationService', { room: result.user_cred._id })
+            
             socket.on('connection', ({ notification }) => {
                 console.log(notification)
                 dispatch(addnotification(notification))
